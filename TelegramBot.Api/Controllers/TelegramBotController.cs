feeb5694605b077c29b1fa57b92d310906575df1
alialijani77 <alialijani1377@gmail.com>
2025-09-20 12:11:03 +0330

    add controller and references

---
+++ TelegramBot.Api/Controllers/TelegramBotController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TelegramBot.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TelegramBotController : ControllerBase
+    {
+        //[AllowAnonymous]
+        //[HttpPost("TelegramBot")]
+        //public async Task<IActionResult> TelegramBot([FromBody] Telegram.Bot.Types.Update update)
+        //{
+        //    return Ok();
+        //}
+    }
+}
