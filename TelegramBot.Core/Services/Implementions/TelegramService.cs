98a2bf87b440766fec2cfc7b16c58de6d9f611fd
alialijani77 <alialijani1377@gmail.com>
2025-09-21 14:15:13 +0330

    add StartMessageAsync method

--- TelegramBot.Core/Services/Implementions/TelegramService.cs
+++ TelegramBot.Core/Services/Implementions/TelegramService.cs
@@ -1,12 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Telegram.Bot.Types;
+using TelegramBot.Core.DTOs.ApiResult;
+using TelegramBot.Core.Services.Interfaces;
 
 namespace TelegramBot.Core.Services.Implementions
 {
-    internal class TelegramService
+    public class TelegramService : ITelegramService
     {
+        public Task<ApiResultDto<string>> StartMessageAsync(Update telegramBotDto, CancellationToken ct)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- TelegramBot.Core/Services/Interfaces/ITelegramService.cs
+++ TelegramBot.Core/Services/Interfaces/ITelegramService.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Telegram.Bot.Types;
+using TelegramBot.Core.DTOs.ApiResult;
 
 namespace TelegramBot.Core.Services.Interfaces
 {
-    internal class ITelegramService
+    public interface ITelegramService
     {
+        Task<ApiResultDto<string>> StartMessageAsync(Update telegramBotDto, CancellationToken ct);
     }
 }
