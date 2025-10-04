d15eeda4785f7516955fb1bc78dec7ef0a15f4dd
alialijani77 <alialijani1377@gmail.com>
2025-10-04 15:38:59 +0330

    Fixed ServiceProvider

--- TelegramBot.Domain/Entities/ServiceProvider.cs
+++ TelegramBot.Domain/Entities/ServiceProvider.cs
@@ -2,6 +2,7 @@
 {
     public class ServiceProvider
     {
+        public long ServiceProviderId { get; set; }
         public string? Token { get; set; }
         public string? BotWebhookUrl { get; set; }
     }
---
+++ TelegramBot.Infra.Data/Configurations/ServiceProviderConfig.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TelegramBot.Domain.Entities;
+
+namespace TelegramBot.Infra.Data.Configurations
+{
+    public class ServiceProviderConfig : IEntityTypeConfiguration<ServiceProvider>
+    {
+        public void Configure(EntityTypeBuilder<ServiceProvider> builder)
+        {
+            builder.HasKey(c => c.ServiceProviderId);
+
+        }
+    }
+}
